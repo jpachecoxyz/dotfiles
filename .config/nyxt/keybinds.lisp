ee2e97e293f866c1ad8b7271ee37bb2e54c8c58e
Javier Pacheco <jpacheco@cock.li>
2025-03-03 20:53:04 -0600

    update dots

--- .config/nyxt/keybinds.lisp
+++ .config/nyxt/keybinds.lisp
@@ -6,10 +6,10 @@
     (keymaps:define-keyscheme-map
         "custom" (list :import %slot-value%)
       nyxt/keyscheme:vi-normal
-      (list "C-c e V" 'mpv-video-current-page
-            "C-c e v" 'mpv-video-hint-url
-            "C-c e A" 'mpv-audio-current-page
-            "C-c e a" 'mpv-audio-hint-url
+      (list "C-c o V" 'mpv-video-current-page
+            "C-c o v" 'mpv-video-hint-url
+            "C-c o A" 'mpv-audio-current-page
+            "C-c o a" 'mpv-audio-hint-url
             "C-c e u" 'nyxt/mode/password:copy-username
             "C-c e p" 'nyxt/mode/password:copy-password
             "C-c e o" 'nyxt/mode/visual:visual-mode)))))
